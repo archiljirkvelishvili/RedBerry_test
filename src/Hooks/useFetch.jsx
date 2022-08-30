d9d8e1bb2285691f912fbddf254b0c6e4fbea257
Archil Jirkvelishvili <archil.j@gmail.com>
2022-08-30 03:17:49 -0700

    feat: usefetch added brands dan cpus api

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -1,15 +1,21 @@
 import {useEffect, useState}from "react";
 
 export default function useFetch(){
-    const [fechedData, setFechedData] = useState({teams: {}, positions: {}})
+    const [fechedData, setFechedData] = useState({teams: {}, positions: {}, brands: {}, cpus: {}})
     useEffect(() =>{
         try{
             (async() => {
                 const responeTeams = await fetch("https://pcfy.redberryinternship.ge/api/teams")
                 const responePositions = await fetch("https://pcfy.redberryinternship.ge/api/positions")
+                const responseBrands = await fetch("https://pcfy.redberryinternship.ge/api/brands")
+                const responeCpus = await fetch("https://pcfy.redberryinternship.ge/api/cpus")
+
                 const fetchedTeams = await responeTeams.json()
                 const fetchedPositions =  await responePositions.json()
-                setFechedData(()=>({teams: fetchedTeams, positions: fetchedPositions}))
+                const fetchedBrands = await responseBrands.json()
+                const fetchedCpus = await responeCpus.json()
+
+                setFechedData(()=>({teams: fetchedTeams, positions: fetchedPositions, brands:fetchedBrands, cpus:fetchedCpus}))
                 
             })()
         }catch(e){
--- src/pages/registercomp/RegisterComp.jsx
+++ src/pages/registercomp/RegisterComp.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import {Link} from "react-router-dom"
 
+import useFetch from "../../Hooks/useFetch";
+
 export default function RegisterComp(){
+    const {brands, cpus} = useFetch()
+    
     return(
         <div>
             <Link to="/registerperson" > უკან დაბრუნება </Link>
